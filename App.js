353516a232b6710d1629ad2a2a803ff9f4b8dd9c
Azfar Bilal <azfarbilal@Azfars-MacBook-Pro.local>
2020-10-03 13:57:24 +0500

    nada

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row", //horizontal
+        justifyContent: "center", //main
+        alignItems: "center", //secondary axis
+        alignContent: "center",
+        flexWrap: "wrap",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+          top: 20,
+          left: 20,
+          position: "absolute",
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+    </View>
+  );
+}
